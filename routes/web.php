5f17ef30c3d82e0055493e90ceb79c75f2d740dd
Riemori <nishishinjuku.rie@gmail.com>
2024-09-12 17:05:44 +0900

    共通レイアウトの修正その2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +30,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::resource('reports', ReportController::class)
+    ->only(['index', 'create', 'store', 'show', 'edit', 'update']);
+
+Route::resource('users', UserController::class)
+    ->only(['index', 'edit', 'update']);
